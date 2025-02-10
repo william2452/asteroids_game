db7452687137ce628a1a857d64d832bf7566ddee
william2452 <wmcclean64@gmail.com>
2025-02-09 21:28:33 -0800

    added the player icon, as well as the player's hitbox

--- main.py
+++ main.py
@@ -1,17 +1,26 @@
 import pygame
 from constants import *
+from circleshape import *
+from player import *
 
 def main():
     pygame.init()
+
+    pygame.time.Clock()
+    dt = 0
+
+    player = Player((SCREEN_WIDTH / 2), (SCREEN_WIDTH / 2), PLAYER_RADIUS)
     
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
+        dt = pygame.time.Clock().tick(60) / 1000
+        
 
 if __name__ == "__main__":
     main()
