e983c2a0bbae33f9599bb4d2eaa7c6444ed20a07
william2452 <wmcclean64@gmail.com>
2025-02-09 20:34:41 -0800

    added code to create game window and created .gitignore file

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 import pygame
-import constants
+from constants import *
 
 def main():
-    print("'Starting asteroids!'")
+    pygame.init()
+    
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
-    print("Screen width:", constants.SCREEN_WIDTH)
-    print("Screen height:", constants.SCREEN_HEIGHT)
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill("black")
+        pygame.display.flip()
 
 if __name__ == "__main__":
     main()
